53d05384eab8bc5db46b4844c29179f8c1d12c1f
Pablo-D-P-C <pablodanielperalta@hotmail.com>
2020-04-27 13:10:01 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Today Is A BeautiFul DAY")
+print("Today Is A BeautiFul DAY".lower())
+l = """Where would this come handy? For example if you ask user 'Would you like to continue (yes/no)?', the user might 
+respond: 'yes', 'Yes', 'YES' or even 'YeS'. In this case, changing your user's response into lowercase letters would be 
+very helpful in your if-else statement."""
+print(l.lower())
